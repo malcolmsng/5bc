cc06c68aefd32d9b1feea97fd36904ad27c0d4a5
phoeni <phoenikelly@gmail.com>
2021-06-14 14:25:28 +0800

    vj kinda sux

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>boohoo</Text>
+      <Text>i hate vj</Text>
       <StatusBar style="auto" />
     </View>
   );
