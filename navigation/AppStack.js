1c0826be8a43cebac34f7bc23c62f51810359964
myc37 <nvjn37@gmail.com>
2021-06-15 04:59:26 +0800

    navigation reworks

---
+++ navigation/AppStack.js
@@ -0,0 +1,187 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "../screens/HomeScreen";
+import MyProfileScreen from "../screens/MyProfileScreen";
+import ViewPostScreen from "../screens/ViewPostScreen";
+import ExploreScreen from "../screens/ExploreScreen";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import CreatePostScreen from "../screens/CreatePostScreen";
+import { Button } from 'react-native-paper';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = ({ navigation }) => {
+    return (
+    <Stack.Navigator>
+        <Stack.Screen 
+            name="Home" 
+            component={HomeScreen}
+            options={{
+                headerTitleAlign: 'center',
+                headerTitleStyle: {
+                    color: "rgb(79, 175, 233)",
+                    fontSize: 16,
+                },
+                headerStyle: {
+                    shadowColor: '#fff',
+                    elevation: 0,
+                },
+                // headerRight: () => (
+                //     <View style={{marginRight: 10}}>
+                //         <FontAwesome.Button 
+                //             name="plus"
+                //             size={22}
+                //             backgroundColor="#fff"
+                //             color="rgb(79, 175, 233)"
+                //             onPress={() => navigation.navigate('Create Post')}
+                //         />
+                //     </View>
+                // ),
+                headerRight: () => (
+                    <View style={{marginRight: 10}}>
+                        <FontAwesome.Button 
+                            name="user-circle-o"
+                            size={22}
+                            backgroundColor="#fff"
+                            color="rgb(79, 175, 233)"
+                            onPress={() => navigation.navigate('My Profile')}
+                        />
+                    </View>
+                ),
+            }}
+        />
+        <Stack.Screen 
+            name="My Profile"
+            component={MyProfileScreen}
+            options={{
+                headerTitleAlign: 'center',
+                headerTitleStyle: {
+                    color: "rgb(79, 175, 233)",
+                    fontSize: 16,
+                },
+                headerStyle: {
+                    shadowColor: '#fff',
+                    elevation: 0,
+                },
+                headerBackTitleVisible: false,
+                headerBackImage: () => (
+                    <View style={{marginRight: 10}}>
+                        <FontAwesome 
+                            name="chevron-left"
+                            size={22}
+                            color="rgb(79, 175, 233)"
+                        />
+                    </View>
+                ),
+            }}
+        />
+        <Stack.Screen 
+            name="View Post"
+            component={ViewPostScreen}
+            options={{
+                headerTitleAlign: 'center',
+                headerTitleStyle: {
+                    color: "rgb(79, 175, 233)",
+                    fontSize: 16,
+                },
+                headerStyle: {
+                    shadowColor: '#fff',
+                    elevation: 0,
+                },
+                headerBackTitleVisible: false,
+                headerBackImage: () => (
+                    <View style={{marginRight: 10}}>
+                        <FontAwesome 
+                            name="arrow-left"
+                            size={22}
+                            color="rgb(79, 175, 233)"
+                        />
+                    </View>
+                ),
+            }}
+        />
+    </Stack.Navigator>
+    )
+}
+
+const CreateStack = ({ navigation }) => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="Create Post"
+                component={CreatePostScreen}
+                options={{
+                    headerTitleAlign: 'center',
+                    headerTitleStyle: {
+                        color: "rgb(79, 175, 233)",
+                        fontSize: 16,
+                    },
+                    headerStyle: {
+                        shadowColor: '#fff',
+                        elevation: 0,
+                    },
+                    headerRight: () => (
+                        <View style={{marginRight: 10}}>
+                            <Button 
+                                mode="text"
+                                size={22}
+                                //backgroundColor="#fff"
+                                color="rgb(79, 175, 233)"
+                                onPress={() => console.log("reset")}
+                            >
+                                Reset
+                            </Button>
+                        </View>
+                    ),
+                    //headerBackTitleVisible: false,
+                    headerLeft: () => (
+                        <View style={{marginLeft: 10}}>
+                        <FontAwesome.Button
+                            name="close"
+                            size={22}
+                            backgroundColor="white"
+                            color="rgb(79, 175, 233)"
+                            onPress={() => navigation.navigate('HomeTab')}
+                        />
+                    </View>
+                    ),
+                }}
+            />
+        </Stack.Navigator>
+    )
+}
+
+const AppStack = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "HomeTab") {
+              iconName = "home";
+            } else if (route.name === "Create Post") {
+                iconName = "plus";
+            } else if (route.name === "Explore") {
+                iconName = "search"
+            }
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "rgb(79, 175, 233)",
+          showLabel: false,
+          keyboardHidesTabBar: true
+        }}
+      >
+        <Tab.Screen name="HomeTab" component={HomeStack} />
+        <Tab.Screen name="Create Post" component={CreateStack}/>
+        <Tab.Screen name="Explore" component={ExploreScreen} />
+      </Tab.Navigator>
+    )
+}
+
+export default AppStack
