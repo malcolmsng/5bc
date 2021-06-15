69442818d66c42b594788628af2795b704e36a14
softmagnet <59001819+softmagnet@users.noreply.github.com>
2021-06-15 10:54:56 +0800

    Revert "set up firebase, added login, sign up screens"

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-import * as React from 'react';
+import React from 'react'
 import Routes from './navigation/Routes'
 
-import { AuthProvider } from './contexts/AuthContext'
-import AppNavigation from './AppNavigation'
-
-
 export default function App() {
-  return (
-    <AuthProvider>
-      <AppNavigation />
-    </AuthProvider>
-  );
+    return (
+        <Routes />
+    )
 }
